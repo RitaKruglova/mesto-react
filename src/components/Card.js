518c88fcb85b8f8123d9c81af77c98a955a859b8
Rita Kruglova <kruglova404@mail.ru>
2023-05-11 15:25:01 +0300

    feat: add card component

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+function Card(props) {
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
+
+  return (
+    <article className="card">
+      <img className="card__image" src={props.card.link} alt="#" onClick={handleClick} />
+      <button type="button" className="card__recycle-bin" aria-label="Удалить карточку"></button>
+      <div className="card__caption">
+        <h2 className="card__title">{props.card.name}</h2>
+        <div className="card__like-counter">
+          <button type="button" className="card__like" aria-label="Нравится"></button>
+          <p className="card__counter">{props.card.likes.length > 0 ? `${props.card.likes.length}` : ''}</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Card;
