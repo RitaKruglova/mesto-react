c723cdd254c63378913bcc8e37af9c415211096f
Rita Kruglova <kruglova404@mail.ru>
2023-05-27 17:26:13 +0300

    refactor: add component EditAvatarPopup

---
+++ src/components/EditAvatarPopup.js
@@ -0,0 +1,37 @@
+import PopupWithForm from "./PopupWithForm";
+import { useRef } from 'react';
+
+function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
+  const inputAvatarRef = useRef();
+  const formRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onUpdateAvatar({
+      avatar: inputAvatarRef.current.value
+    });
+
+    onClose();
+    formRef.current.reset();
+  }
+
+  return (
+    <PopupWithForm formRef={formRef} onSubmit={handleSubmit} name="avatar" title="Обновить аватар" isOpen={isOpen} onClose={onClose} >
+      <fieldset className="popup__info">
+        <input
+          id="input-link-avatar"
+          type="url"
+          name="avatar-link"
+          className="popup__input popup__input_type_avatar"
+          placeholder="Ссылка на картинку"
+          required
+          ref={inputAvatarRef}
+        />
+        <span id="input-link-avatar-error" className="popup__error popup__error_visible"></span>
+      </fieldset>
+    </PopupWithForm>
+  )
+}
+
+export default EditAvatarPopup;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,7 +6,7 @@ function PopupWithForm(props) {
       <div className={`popup__container popup__container_place_${props.name}`}>
         <button type="button" className="popup__reset-button" aria-label="Отменить" onClick={props.onClose}></button>
         <h3 className="popup__title">{props.title}</h3>
-        <form onSubmit={props.onSubmit} className="popup__form" name={props.name}>
+        <form ref={props.formRef} onSubmit={props.onSubmit} className="popup__form" name={props.name}>
           {props.children}
           <button type="submit" className="popup__submit-button">{props.buttonText || 'Сохранить'}</button>
         </form>
